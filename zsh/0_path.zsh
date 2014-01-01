4c5d58d7c65baf873e4464f6e0cf0b41bb9e5a79
hunterboerner <hunterboerner@gmail.com>
2014-01-01 12:03:33 -0600

    fix cuda stuff

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -1,6 +1,4 @@
 # path, the 0 in the filename causes this to load first
-export PATH=/Developer/NVIDIA/CUDA-5.5/bin:$PATH
-export DYLD_LIBRARY_PATH=/Developer/NVIDIA/CUDA-5.5/lib:$DYLD_LIBRARY_PATH
 export PATH=/opt/local/bin:/opt/local/sbin:$PATH
 export PATH=$PATH:$HOME/.magus/bin:$HOME/.magus/bin/magus
 export PATH=$PATH:~/bin
